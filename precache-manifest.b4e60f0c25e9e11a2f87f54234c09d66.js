e123bf135fa33ef5f5a4ddde06ce40fdf16afcd5
Alexey-Arnaut <arnaut.aleksey_69@mail.ru>
2021-10-12 22:09:03 +0500

    Updates

---
+++ precache-manifest.b4e60f0c25e9e11a2f87f54234c09d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233df18c08218595289646ee5b53a936",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "238bc081e572f39a0046",
+    "url": "/react-weather/static/css/main.48304884.chunk.css"
+  },
+  {
+    "revision": "f14e82de8fc7dfdc7541",
+    "url": "/react-weather/static/js/2.cc300775.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-weather/static/js/2.cc300775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "238bc081e572f39a0046",
+    "url": "/react-weather/static/js/main.00a80242.chunk.js"
+  },
+  {
+    "revision": "2f0b080eedbebc4ebf61",
+    "url": "/react-weather/static/js/runtime-main.10c92b92.js"
+  }
+]);
